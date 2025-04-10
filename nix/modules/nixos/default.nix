64f0a1cb3fa03e44e8457018f7358346f0b83ca4
Jeff Hutchison <jeff@jeffhutchison.com>
2025-04-10 14:00:43 -0400

    Add module options

--- nix/modules/nixos/default.nix
+++ nix/modules/nixos/default.nix
@@ -6,45 +6,70 @@
   ...
 }:
 let
+  cfg = config.j3ff.watchtower.prometheus-speedtest-collector;
   inherit (pkgs) system;
 in
 {
 
-  systemd.services.prometheus-speedtest-collector = {
-    description = "Prometheus Speedtest collector";
-    environment = {
-      PROMETHEUS_TEXTFILE_DIR = "/var/local/prometheus/node-exporter";
+  options = {
+    j3ff.watchtower.prometheus-speedtest-collector = {
+      enable = lib.mkEnableOption "Prometheus speedtest collection";
+
+      interval = lib.mkOption {
+        type = lib.types.str;
+        default = "24h";
+        description = ''
+          OnUnitActiveSec specification for collection.
+        '';
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    systemd.timers.prometheus-speedtest-collector = {
+      description = "Run Prometheus speedtest collector periodically";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnUnitActiveSec = cfg.interval;
+        OnBootSec = "30m";
+      };
     };
 
-    startAt = "*-*-* 06,18:23:00";
-    serviceConfig = {
-      ExecStart = ''
-        ${flake.packages.${system}.default}/bin/prometheus-speedtest-collector
-      '';
-      Restart = "on-failure";
+    systemd.services.prometheus-speedtest-collector = {
+      description = "Prometheus speedtest collector";
+      environment = {
+        PROMETHEUS_TEXTFILE_DIR = "/var/local/prometheus/node-exporter";
+      };
 
-      User = "node-exporter";
+      serviceConfig = {
+        Type = "oneshot";
+        Restart = "no";
+        ExecStart = ''
+          ${flake.packages.${system}.default}/bin/prometheus-speedtest-collector
+        '';
 
-      BindReadOnlyPaths = [
-        "${
-          config.environment.etc."ssl/certs/ca-certificates.crt".source
-        }:/etc/ssl/certs/ca-certificates.crt"
-        builtins.storeDir
-        "-/etc/resolv.conf"
-        "-/etc/nsswitch.conf"
-        "-/etc/hosts"
-        "-/etc/localtime"
-      ];
+        User = "node-exporter";
 
-      RestrictAddressFamilies = "AF_UNIX AF_INET";
-      CapabilityBoundingSet = "";
-      SystemCallFilter = [
-        "@system-service"
-        "~@privileged @setuid @keyring"
-      ];
+        BindReadOnlyPaths = [
+          "${
+            config.environment.etc."ssl/certs/ca-certificates.crt".source
+          }:/etc/ssl/certs/ca-certificates.crt"
+          builtins.storeDir
+          "-/etc/resolv.conf"
+          "-/etc/nsswitch.conf"
+          "-/etc/hosts"
+          "-/etc/localtime"
+        ];
+
+        RestrictAddressFamilies = "AF_UNIX AF_INET";
+        CapabilityBoundingSet = "";
+        SystemCallFilter = [
+          "@system-service"
+          "~@privileged @setuid @keyring"
+        ];
+      };
     };
-
-    wantedBy = [ "multi-user.target" ];
   };
 
 }
